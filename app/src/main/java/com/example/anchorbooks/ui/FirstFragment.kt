c6bf6aea6dcad935ab0e853da1495c5d32878560
Miguel <miguel_ahumada_c@hotmail.com>
2021-02-27 13:03:39 -0300

    se crea el item_book_detail.xml

--- app/src/main/java/com/example/anchorbooks/ui/FirstFragment.kt
+++ app/src/main/java/com/example/anchorbooks/ui/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.example.anchorbooks
+package com.example.anchorbooks.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,25 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.anchorbooks.R
+import com.example.anchorbooks.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
+class FirstFragment : Fragment() {
+    private lateinit var binding: FragmentFirstBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 }
